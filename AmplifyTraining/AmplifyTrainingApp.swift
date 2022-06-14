6cfc6ec4d2775a55bd47998f9b140278b78f051e
Shimamura Seiya <shimamura.seiya@classmethod.jp>
2022-06-14 12:44:53 +0900

    Initial Commit

---
+++ AmplifyTraining/AmplifyTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmplifyTrainingApp.swift
+//  AmplifyTraining
+//
+//  Created by shimamura.seiya on 2022/06/14.
+//
+
+import SwiftUI
+
+@main
+struct AmplifyTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
